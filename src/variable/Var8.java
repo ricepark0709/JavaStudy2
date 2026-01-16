464472cebe96f72615031406326fb7cc0673a5b0
ricepark0709 <juncomputer0709@gmail.com>
2026-01-16 17:14:44 +0900

    변수 타입

---
+++ src/variable/Var8.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 10L;
+
+        // 실수
+        double d;
+        float f = 10.0f;
+
+        // 정수는 int와 long, 실수는 double, boolean, 문자는 String을 사용
+    }
+}

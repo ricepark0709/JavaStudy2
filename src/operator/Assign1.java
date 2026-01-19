5ce5f57324e62e3f5e3a82b954b5fc57fb113324
ricepark0709 <juncomputer0709@gmail.com>
2026-01-20 08:32:02 +0900

    논리 연산자, 대입 연산자, 퀴즈

---
+++ src/operator/Assign1.java
@@ -0,0 +1,16 @@
+package operator;
+
+import java.util.Arrays;
+
+public class Assign1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a += 3; // 8
+        a -= 2; // 6
+        a *= 4; // 24
+        a /= 3; // 8
+        a %= 5; // 3
+        System.out.println(a);
+    }
+}

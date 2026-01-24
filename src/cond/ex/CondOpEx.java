ad7150af7c20a033177c431f28aaceff6e1a22e3
ricepark0709 <juncomputer0709@gmail.com>
2026-01-24 12:20:13 +0900

    퀴즈, 반복문

---
+++ src/cond/ex/CondOpEx.java
@@ -0,0 +1,9 @@
+package cond.ex;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int c = (a > b) ? a : b;
+        System.out.println("더 큰 숫자는 " + c + "입니다.");
+    }
+}

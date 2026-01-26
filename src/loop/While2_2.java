4d3a343e70236a211d877bdfe535d6d09da50fb5
ricepark0709 <juncomputer0709@gmail.com>
2026-01-26 12:28:50 +0900

    do-while, break, continue, for

--- src/loop/While2_2.java
+++ src/loop/While2_2.java
@@ -5,7 +5,6 @@ public class While2_2 {
     public static void main(String[] args) {
         int sum = 0;
         int i = 100;
-        ;
 
         sum = sum + i;
         System.out.println("i = " + i + " sum = " + sum);

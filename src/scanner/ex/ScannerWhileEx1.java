6629e2bf8e25535c351592d6653d4530d7850810
ricepark0709 <juncomputer0709@gmail.com>
2026-02-02 13:19:26 +0900

    ScannerWhile 퀴즈

--- src/scanner/ex/ScannerWhileEx1.java
+++ src/scanner/ex/ScannerWhileEx1.java
@@ -10,12 +10,14 @@ public class ScannerWhileEx1 {
             System.out.print("이름을 입력하세요 (종료를 입력하면 종료): ");
             String name = input.nextLine();
 
-            System.out.print("나이를 입력하세요: ");
-            int age = input.nextInt();
-
-            if (name == "종료") {
+            if (name.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
                 break;
             }
+
+            System.out.print("나이를 입력하세요: ");
+            int age = input.nextInt();
+            input.nextLine();
 
             System.out.println("입력한 이름: " + name + ", 나이: " + age);
         }

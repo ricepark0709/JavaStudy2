6f84101a82553a335c76f0cfab04cb24db3a718c
ricepark0709 <juncomputer0709@gmail.com>
2026-02-03 13:32:54 +0900

    배열

--- src/scanner/ex/ScannerWhileEx4.java
+++ src/scanner/ex/ScannerWhileEx4.java
@@ -14,18 +14,22 @@ public class ScannerWhileEx4 {
 
             if (option == 1) {
                 input.nextLine();
-                System.out.println("상품명을 입력하세요: ");
+                System.out.print("상품명을 입력하세요: ");
                 String product = input.nextLine();
 
-                System.out.println("상품의 가격을 입력하세요: ");
+                System.out.print("상품의 가격을 입력하세요: ");
                 int price = input.nextInt();
 
-                System.out.println("구매 수량을 입력하세요: ");
+                System.out.print("구매 수량을 입력하세요: ");
                 int quantity = input.nextInt();
+
+                totalCost += price * quantity;
+                System.out.println("상품명: " + product + " 가격: " + price + " 수량: " + quantity + " 합계: " + price * quantity);
 
 
             } else if (option == 2) {
-
+                System.out.println("총 비용: " + totalCost);
+                totalCost = 0;
             } else if (option == 3) {
                 System.out.println("프로그램을 종료합니다.");
                 break;

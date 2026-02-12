d5054e59c606fd0487150ba42dcd4329ac596f26
ricepark0709 <juncomputer0709@gmail.com>
2026-02-12 13:41:41 +0900

    메서드 퀴즈

--- src/method/ex/MethodEx4.java
+++ src/method/ex/MethodEx4.java
@@ -6,24 +6,26 @@ public class MethodEx4 {
 
     public static void main(String[] args) {
         int balance = 0;
-
-        System.out.println("---------------------------------");
-        System.out.println("1.입금 | 2.출금 | 3.잔액 확인 | 4.종료");
-        System.out.println("---------------------------------");
-
-        System.out.print("선택: ");
         Scanner scanner = new Scanner(System.in);
-        int menu = scanner.nextInt();
 
         while (true) {
+            System.out.println("---------------------------------");
+            System.out.println("1.입금 | 2.출금 | 3.잔액 확인 | 4.종료");
+            System.out.println("---------------------------------");
+
+            System.out.print("선택: ");
+            int menu = scanner.nextInt();
+
             if (menu == 1) {
+                System.out.print("입금액을 입력하세요: ");
                 int depositMoney = scanner.nextInt();
                 balance = deposit(balance, depositMoney);
             } else if (menu == 2) {
+                System.out.print("출금액을 입력하세요: ");
                 int withdrawMoney = scanner.nextInt();
                 balance = withdraw(balance, withdrawMoney);
             } else if (menu == 3) {
-
+                System.out.println("현재 잔액: " + balance + "원");
             } else if (menu == 4) {
                 break;
             }
